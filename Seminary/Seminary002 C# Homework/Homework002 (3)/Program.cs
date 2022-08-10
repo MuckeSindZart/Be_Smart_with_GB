1bbab1367198d3c17b500b06020c45f574d25aae
Mucke <muckesindzart@gmail.com>
2022-08-10 16:46:05 +0600

    Seminar002 C# Homework002 (3) New fix

--- Seminary/Seminary002 C# Homework/Homework002 (3)/Program.cs
+++ Seminary/Seminary002 C# Homework/Homework002 (3)/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+Console.WriteLine($"Hello, World! Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+System.Console.WriteLine("PS. числа от от -2147483647 до 2147483647");
 
 /*
 Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
@@ -8,7 +9,10 @@ Console.WriteLine("Hello, World! Программа, которая выводи
 32679 -> 6
 */
 
-int Prompt(string message)
+
+
+
+int Prompt(string message)  //Ввод числа от -2147483648 до 2147483647
 {
     System.Console.Write(message);
     string input = System.Console.ReadLine();
@@ -17,104 +21,123 @@ int Prompt(string message)
 }
 
 
-//сколько цифр в числе 
-int SizeOfNumber(int a)
+
+int SizeOfNumber(int a)  //счетчик сколько цифр в числе для варианта № 1
 {
     int size = 1;
-    int i = 9;
+    double i = 9;
 
-    //если - поменять знак на +
-    if (a < 0) { a = -a; }
+    if (a < 0)
+    { a = -a; }   //если - поменять знак на +
 
-    // если >= 99 size++
-    while (a > i)
-
+    while (a > i)       // если >= 99 size++
     {
         i = i * 10 + 9;
         size++;
     }
+
     return size;
-
 }
-/*
-//сколько цифр в числе вариант 2
-int sizeOfNumber(int a)                                  
-{
-int size = 0;
-int i = 1;
-if (a < 0) { a = -a; }
-while (a != 0)
-{
-i = i * 10;
-a = a - a % i;
-size++;
-}
-if (a >= 0 && size == 0) { size = 1; }
-return size;
-}*/
 
-//заполнить массив с конца 
-int[] FillArray(int number, int size)
+
+
+int[] FillArray(int number, int size)   //заполнение массива начиная с конца, для варианта № 2
 {
     int[] array = new int[size];
     while (size > 0)
     {
-        array[size - 1] = number % 10;
-        number = (number - number % 10) / 10;
         size--;
-
+        array[size] = number % 10;
+        number = (number - number % 10) / 10;
     }
     return array;
 }
 
-//печать массива
-void PrintArray(int[] array)
+
+void PrintArray(int[] array)   //печать массива в строку как будто это число, для варианта № 2
 {
+
     int count = array.Length;
-    int i = 0;
+    int i = 1;
+    System.Console.Write(array[0]);
     while (i < count)
     {
-        System.Console.Write(array[i]);
+        if (array[0] < 0)
+        {
+            System.Console.Write(-array[i]);
+        }
+        else
+        {
+            System.Console.Write(array[i]);
+        }
         i++;
     }
 }
 
-start:
+start: //возврат для ввода если ошибка
 Console.ForegroundColor = ConsoleColor.White;
 
 
 int num = Prompt("Введите число : ");
-int sizenum = SizeOfNumber(num);
-int[] arrayNum = FillArray(num, sizenum);
+
+int sizenum = SizeOfNumber(num); //№1
+int[] arrayNum = FillArray(num, sizenum);//№2
+int result = num; //вывод резу
+
 
 //Ошибка, меньше 3 цифр
 if (sizenum <= 2)
 {
     Console.ForegroundColor = ConsoleColor.Red;
     System.Console.WriteLine($"{num}-> Ошибка, третьей цифры нет");
+
     goto start;
 }
 
 
-//вывод третьей цифры заданного числа
+
+//вывод третьей цифры заданного числа, вариант № 1
+
+while (sizenum > 3)
+{
+    result = (result - (result % 10)) / 10;
+    sizenum--;
+}
+result = result % 10;
+System.Console.WriteLine($"{num}->{result}");
+
+
+
+// вариант с выводом массива как числа, вариант №2
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+PrintArray(arrayNum);
+System.Console.WriteLine($"->{arrayNum[2]}");
+
+
+
+// вариант с преносом запятой и преобразованием в int, вариант №3
+
+double doubleNum = num;
+
+if (num < 0)
+{
+    while (doubleNum < -1000)
+    {
+        doubleNum = doubleNum * 0.1;
+
+    }
+}
+
 else
 {
-    System.Console.Write($"{num}->");
-    int result = num;
-
-    while (sizenum > 3)
+    while (doubleNum > 1000)
     {
-        result = (result - (result % 10)) / 10;
-        sizenum--;
+        doubleNum = doubleNum * 0.1;
+
     }
-    result = result % 10;
-    System.Console.WriteLine($"{result}");
-
-
-    // вариант с массивом
-
-    Console.ForegroundColor = ConsoleColor.Yellow;
-    PrintArray(arrayNum);
-    System.Console.WriteLine($"->{arrayNum[2]}");
-
 }
+result = (int)doubleNum % 10;
+
+Console.ForegroundColor = ConsoleColor.Green;
+System.Console.WriteLine($"{num}->{result}");

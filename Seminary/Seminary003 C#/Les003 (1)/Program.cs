a44db626f8a10d2a2f90198a6f92880cd7521863
Mucke <muckesindzart@gmail.com>
2022-08-12 01:29:52 +0600

    Seminar003 C# Les003 (1) номер четверти плоскости, в которой находится точка Les003 (2) расстояние между ними в 2D пространстве. Les003 (3) таблица квадратов чисел от 1 до N.

---
+++ Seminary/Seminary003 C#/Les003 (1)/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! Программа, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 или Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.");
+
+/*
+Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 или Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+*/
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string input = System.Console.ReadLine();
+    int result = int.Parse(input);
+    return result;
+}
+
+int printXY(float x, float y)
+{
+    System.Console.Write($"X={x},Y={y} ");
+    
+    if (x > 0 && y > 0) { return 1; }
+
+    else if (x < 0 && y > 0) { return 2; }
+
+    else if (x < 0 && y < 0) { return 3; }
+
+    else if (x > 0 && y < 0) { return 4; }
+
+    else { return -1; }
+}
+
+void printSetXY(int num)
+{
+    if (num == 1)
+    {
+        Console.WriteLine("Плоскость - 1");
+        System.Console.WriteLine("X > 0, Y > 0");
+    }
+    if (num == 2)
+    {
+        Console.WriteLine("Плоскость - 2");
+        System.Console.WriteLine("X < 0, Y > 0");
+    }
+    if (num == 3)
+    {
+        Console.WriteLine("Плоскость - 3");
+        System.Console.WriteLine("X < 0, Y < 0");
+    }
+    if (num == 4)
+    {
+        Console.WriteLine("Плоскость - 4");
+        System.Console.WriteLine("X > 0, Y < 0");
+    }
+    if (num == -1)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Ошибка кординат!");
+    }
+}
+
+
+
+start:
+
+Console.ForegroundColor = ConsoleColor.White;
+int result = printXY(Prompt("Введите X :"), Prompt("Введите Y :"));
+
+if (result < 0) { goto start; }
+
+printSetXY(result);
+
+System.Console.WriteLine();
+
+
+
+
---
+++ Seminary/Seminary003 C#/Les003 (2)/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! Программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.");
+System.Console.WriteLine("A (3,6); B (2,1) -> 5,09      A (7,-5); B (1,-1) -> 7,21");
+
+/*
+Задача 3: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+A (3,6); B (2,1) -> 5,09 
+A (7,-5); B (1,-1) -> 7,21
+*/
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string input = System.Console.ReadLine();
+    int result = int.Parse(input);
+    return result;
+}
+
+
+int aX = Prompt("Введите a X : ");
+int aY = Prompt("Введите a Y : ");
+int bX = Prompt("Введите b X : ");
+int bY = Prompt("Введите b Y : ");
+
+double result = Math.Sqrt((aX-bX)*(aX-bX)+(aY-bY)*(aY-bY));
+System.Console.WriteLine("Растояние между этими двумя точками ="+result);
